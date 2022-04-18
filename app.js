675b89adc4e696706eadbb1844abab2b93ec8381
Morgan Harmon <morganharmon@gmail.com>
2022-04-18 14:44:29 -0700

    areatriangle working

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ console.log(`convertToSeconds(10) === 600 : ${convertToSeconds(10) === 600}`);
 // 2. areaOfTriangle
 // Write a function called areaOfTriangle that takes in the base and height
 // of a triangle and returns its area
-function areaOfTriangle() {}
+function areaOfTriangle(base, height) {
+    const area = base * height / 2;
+    return area;
+}
 console.log(`areaOfTriangle(5, 10) === 25 : ${areaOfTriangle(5, 10) === 25}`);
 
 // 3. lessThan100
